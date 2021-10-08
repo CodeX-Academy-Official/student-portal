54bbddef047b8afa5d2608345bbd1e66a9d9b09b
jrmart12 <rodolfojose@live.com>
2021-10-08 17:00:25 -0600

    private route

---
+++ src/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext';
+function PrivateRoute({ component: Component, ...rest }) {
+  const { currentUser } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return currentUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/log-in" />
+        );
+      }}
+    ></Route>
+  );
+}
+
+export default PrivateRoute;
