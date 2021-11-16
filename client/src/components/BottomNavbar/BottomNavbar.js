a0946c8bcf65f377fbc6965d366efb6ced8fb118
jrmart12 <rodolfojose@live.com>
2021-11-16 16:43:41 -0600

    eslint errors

--- client/src/components/BottomNavbar/BottomNavbar.js
+++ client/src/components/BottomNavbar/BottomNavbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-lone-blocks */
 /* eslint-disable no-unreachable */
 import React, { useState, useEffect } from "react";
--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { Layout, Menu } from "antd";
 import logo from "../../img/logoIcon.png";
